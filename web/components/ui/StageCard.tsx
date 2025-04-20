73a11c625e2cf74e4c5d9f663daf57bcb967170f
Ashvin Nair <ashvinnair@gmail.com>
2025-04-21 03:36:51 +0800

    style: Update lock icon stroke width to 0.5 for Locked Stage Cards

--- web/components/ui/StageCard.tsx
+++ web/components/ui/StageCard.tsx
@@ -164,7 +164,7 @@ const StageCard: React.FC<StageCardProps> = ({
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth={2}
+                strokeWidth={0.5}
                 d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
               />
             </svg>
